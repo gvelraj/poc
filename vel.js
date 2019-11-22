1f8ee1abcfdf64a1cd72a97e32cddc9ed026803d
gvelraj <57611402+gvelraj@users.noreply.github.com>
2019-11-22 10:35:57 +0530

    Update vel.js

--- vel.js
+++ vel.js
@@ -42,57 +42,27 @@
     });
   });
 
+  var myVar; 
+  
+  function refreshStart(dataSource){
+	  
+	  myVar = setInterval(function() { refreshDataSource(dataSource); }, 30000);
+  }
+  
+  function refreshStop(){
+	  
+	  clearInterval(myVar); 
+  }
+  
   // Refreshes the given dataSource.
   function refreshDataSource (dataSource) {
-    /*dataSource.refreshAsync().then(function () {
+	  
+    dataSource.refreshAsync().then(function () {
       console.log(dataSource.name + ': Refreshed Successfully');
-    });*/
-	  alert("test -->" + datasource);
+    });
   }
 
-  // Displays a modal dialog with more details about the given dataSource.
-  function showModal (dataSource) {
-    let modal = $('#infoModal');
-
-    $('#nameDetail').text(dataSource.name);
-    $('#idDetail').text(dataSource.id);
-    $('#typeDetail').text((dataSource.isExtract) ? 'Extract' : 'Live');
-
-    // Loop through every field in the dataSource and concat it to a string.
-    let fieldNamesStr = '';
-    dataSource.fields.forEach(function (field) {
-      fieldNamesStr += field.name + ', ';
-    });
-
-    // Slice off the last ", " for formatting.
-    $('#fieldsDetail').text(fieldNamesStr.slice(0, -2));
-
-    dataSource.getConnectionSummariesAsync().then(function (connectionSummaries) {
-      // Loop through each connection summary and list the connection's
-      // name and type in the info field
-      let connectionsStr = '';
-      connectionSummaries.forEach(function (summary) {
-        connectionsStr += summary.name + ': ' + summary.type + ', ';
-      });
-
-      // Slice of the last ", " for formatting.
-      $('#connectionsDetail').text(connectionsStr.slice(0, -2));
-    });
-
-    dataSource.getActiveTablesAsync().then(function (activeTables) {
-      // Loop through each table that was used in creating this datasource
-      let tableStr = '';
-      activeTables.forEach(function (table) {
-        tableStr += table.name + ', ';
-      });
-
-      // Slice of the last ", " for formatting.
-      $('#activeTablesDetail').text(tableStr.slice(0, -2));
-    });
-
-    modal.modal('show');
-  }
-
+ 
   // Constructs UI that displays all the dataSources in this dashboard
   // given a mapping from dataSourceId to dataSource objects.
   function buildDataSourcesTable (dataSources) {
@@ -110,18 +80,25 @@
       let infoCell = newRow.insertCell(2);
 
       let refreshButton = document.createElement('button');
-      refreshButton.innerHTML = ('Refresh Now');
+      refreshButton.innerHTML = ('Start Refresh cycle');
       refreshButton.type = 'button';
       refreshButton.className = 'btn btn-primary';
-      refreshButton.addEventListener('click', function () { refreshDataSource(dataSource); });
+      refreshButton.addEventListener('click', function () { refreshStart(dataSource); });
+	  
+	  let stoprefreshButton = document.createElement('button');
+      stoprefreshButton.innerHTML = ('Stop Refresh');
+      stoprefreshButton.type = 'button';
+      stoprefreshButton.className = 'btn btn-primary';
+      stoprefreshButton.addEventListener('click', function () { refreshStop(); });
 
-      let infoSpan = document.createElement('span');
-      infoSpan.className = 'glyphicon glyphicon-info-sign';
-      infoSpan.addEventListener('click', function () { showModal(dataSource); });
+      //let infoSpan = document.createElement('span');
+      //infoSpan.className = 'glyphicon glyphicon-info-sign';
+      //infoSpan.addEventListener('click', function () { showModal(dataSource); });
 
       nameCell.innerHTML = dataSource.name;
       refreshCell.appendChild(refreshButton);
-      infoCell.appendChild(infoSpan);
+	  infoCell.appendChild(stoprefreshButton);
+      //infoCell.appendChild(infoSpan);
     }
   }
 })();
